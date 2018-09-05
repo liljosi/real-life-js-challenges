d0b7399fa5374f607d0e9e0327ce3ea1dbbd34d6
Josue Del Rio <sandra@MacBook-Pro-de-Sandra-2.local>
2018-09-04 19:44:22 -0500

    removed deleted directories

--- api/middleware/playlists.js
+++
@@ -1,9 +0,0 @@
-const spotifyPlaylistsLib = require('../../lib/spotify/playlists')
-
-const getPlaylist = (req, res, next) => {
-  let {playlist: playlistName} = req.params
-  const playlist = spotifyPlaylistsLib.getUserPlaylistByName(playlistName)
-  res.json(playlist)
-}
-
-module.exports = getPlaylist
--- api/routes/playlists.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const {getPlaylist} = require('../middleware/playlists')
-
-router.route('/user/:id/playlists/:playlist')
-  .get(getPlaylist)
-
-module.exports = router
