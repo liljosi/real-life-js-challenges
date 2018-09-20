fd99d2e87362cfa2504cdda648982cf4c5ca0086
Josue Del Rio <sandra@MacBook-Pro-de-Sandra-2.local>
2018-09-20 15:43:34 -0500

    fixed comment issues

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,26 @@
 var express = require('express')
 var router = express.Router()
 const spotifyLib = require('../lib/spotify/playlists')
-
+const userId = '07eoivmqhpamcrrsil14vfz07'
+const playlistId = '5F2lnUg2du5po7pNMQqEVN'
 const songs = async (userId, playlistId) => {
   try {
     let tracks = await spotifyLib.getPlaylistSongs(userId, playlistId)
     return tracks
   } catch (error) {
-    return error
+    throw error
   }
 }
 
 router.get('/', async (req, res, next) => {
-  let playlistSongs = await songs('07eoivmqhpamcrrsil14vfz07', '5F2lnUg2du5po7pNMQqEVN')
-  res.render('index', { songUrl: playlistSongs.body.tracks.items[0].track.uri, title: 'Challenge' })
+  let playlistSongs = await songs(userId, playlistId)
+  playlistSongs
+    .then(playlistSongs => {
+      res.render('index', { songUrl: playlistSongs.body.tracks.items[0].track.uri, title: 'Challenge' })
+    })
+    .catch(error => {
+      throw new Error(error)
+    })
 })
 
 module.exports = router
